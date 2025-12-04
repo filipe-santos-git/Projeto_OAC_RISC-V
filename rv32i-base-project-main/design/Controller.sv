d7b9aafb6f3bb7422861b4cee33340e02fe8962c
linsgm <antoniolinsgm@outlook.com>
2025-12-04 01:23:41 -0300

    updated version

--- rv32i-base-project-main/design/Controller.sv
+++ rv32i-base-project-main/design/Controller.sv
@@ -19,13 +19,15 @@ module Controller (
     output logic Branch  //0: branch is not taken; 1: branch is taken
 );
 
-  logic [6:0] I_TYPE, R_TYPE, LW, SW, BR;
+  logic [6:0] I_TYPE, R_TYPE, LW, SW, BR, JAL, JALR;
 
   assign I_TYPE = 7'b0010011;   //addi,slti,slli,srli,srai
   assign R_TYPE = 7'b0110011;   //add,and,slt
   assign LW = 7'b0000011;       //lw
   assign SW = 7'b0100011;       //sw
-  assign BR = 7'b1100011;       //beq
+  assign BR = 7'b1100011;       //beq,bne,blt,bge
+  assign JAL = 7'b1101111;      //jal
+  assign JALR = 7'b1100111;     //jalr
 
   assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE);
   assign MemtoReg = (Opcode == LW);
