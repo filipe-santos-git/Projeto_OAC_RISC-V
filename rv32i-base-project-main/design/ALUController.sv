bce2d2859c201a767c27afb83acf7038f351eb4f
linsgm <antoniolinsgm@outlook.com>
2025-12-05 11:05:11 -0300

    updated version

--- rv32i-base-project-main/design/ALUController.sv
+++ rv32i-base-project-main/design/ALUController.sv
@@ -10,31 +10,31 @@ module ALUController (
     output logic [3:0] Operation  // operation selection for ALU
 );
 
-  assign Operation[0] = ((ALUOp == 2'b11)) ||                                                   // JAL
+  assign Operation[0] = ((ALUOp == 2'b11)) ||                                                   // JAL/JALR
                         ((ALUOp == 2'b10) && (Funct3 == 3'b110)) ||                             // OR
                         (ALUOp == 2'b01) && (Funct3 == 3'b001) ||                               // BNE
-                        (ALUOp == 2'b01) && (Funct3 == 3'b101) ||                               // BGE  
+                        (ALUOp == 2'b01) && (Funct3 == 3'b101) ||                               // BGE
                         ((ALUOp == 2'b10) && (Funct3 == 3'b101)) ||                             // SRLI/SRAI
                         ((ALUOp == 2'b10) && (Funct3 == 3'b100));                               // XOR
 
   assign Operation[1] = (ALUOp == 2'b00) ||                                                     // LW/SW
-                        (ALUOp == 2'b01) && (Funct3 == 3'b100) ||                               // BLT  
-                        (ALUOp == 2'b01) && (Funct3 == 3'b101) ||                               // BGE  
+                        (ALUOp == 2'b01) && (Funct3 == 3'b100) ||                               // BLT
+                        (ALUOp == 2'b01) && (Funct3 == 3'b101) ||                               // BGE
                         ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||                             // SUB/ADD/ADDI
                         ((ALUOp == 2'b10) && (Funct3 == 3'b100)) ||                             // XOR
                         ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));     // SRAI
 
-  assign Operation[2] = ((ALUOp == 2'b11)) ||                                                   // JAL
+  assign Operation[2] = ((ALUOp == 2'b11)) ||                                                   // JAL/JALR
                         ((ALUOp == 2'b10) && (Funct3 == 3'b101)) ||                             // SRLI/SRAI
                         ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)) ||   // SUB
                         ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||                             // SLLI
                         ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||                             // SLT/SLTI
                         ((ALUOp == 2'b01) && (Funct3 == 3'b100));                               // BLT
 
-  assign Operation[3] = ((ALUOp == 2'b11)) ||                                                   // JAL
+  assign Operation[3] = ((ALUOp == 2'b11)) ||                                                   // JAL/JALR
                         (ALUOp == 2'b01) && (Funct3 == 3'b000) ||                               // BEQ
                         (ALUOp == 2'b01) && (Funct3 == 3'b001) ||                               // BNE
-                        (ALUOp == 2'b01) && (Funct3 == 3'b100) ||                               // BLT  
-                        (ALUOp == 2'b01) && (Funct3 == 3'b101) ||                               // BGE                                 
+                        (ALUOp == 2'b01) && (Funct3 == 3'b100) ||                               // BLT
+                        (ALUOp == 2'b01) && (Funct3 == 3'b101) ||                               // BGE
                         ((ALUOp == 2'b10) && (Funct3 == 3'b010));                               // SLT/SLTI
 endmodule
